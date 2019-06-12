4c66bea374ce085c16bc6e693e5c7a2d46fcfddf
Your Name <dminones@gmail.com>
2019-06-12 20:38:22 -0300

    using port 80

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var app = express();
 
 app.get('/ejemplo', function(req,res){ res.send("ejemplo con docker")});
 
-app.listen(process.env.PORT, '0.0.0.0');
+app.listen(process.env.PORT || 80, '0.0.0.0');
 module.exports = app;
